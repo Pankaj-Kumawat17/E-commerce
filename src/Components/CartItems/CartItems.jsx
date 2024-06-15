d93e8fc65f2eb81c2f5fd21cca002fc4c0fd574a
Pankaj Kumawat <pjkumawat17@gmail.com>
2024-06-15 18:58:00 +0530

    first commit

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -5,7 +5,7 @@ import remove_icon from '../Assets/cart_cross_icon.png'
 
 
  const CartItems = () => {
-    const {all_product,cartItems,removeFromCart}=useContext(ShopContext);
+    const {getTotalCartAmount,all_product,cartItems,removeFromCart}=useContext(ShopContext);
   return (
     <div className='cartitems'>
         <div className="cartitems-format-main">
@@ -27,12 +27,42 @@ import remove_icon from '../Assets/cart_cross_icon.png'
             <p>${e.new_price}</p>
             <button className='cartitems-quantity'>{cartItems[e.id]}</button>
             <p>${e.new_price*cartItems[e.id]}</p>
-            <img src={remove_icon} onClick={removeFromCart(e.id)} alt="" />
+            <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
         </div>
         <hr />
                 </div>
             }
+            return null;
         })}
+        <div className="cartitems-down">
+            <div className="cartitems-total">
+                <h1>Cart Totals</h1>
+                <div>
+                    <div className="cartitems-total-item">
+                        <p>Subtotal</p>
+                        <p>${getTotalCartAmount()}</p>
+                    </div>
+                    <hr />
+                    <div className="cartitems-total-item">
+                        <p>Shipping Fee</p>
+                        <p>Free</p>
+                    </div>
+                    <hr />
+                    <div className="cartitems-total-item">
+                        <h3>Total</h3>
+                        <h3>${getTotalCartAmount()}</h3>
+                    </div>
+                </div>
+                <button>PROCEED TO CHECKOUT</button>
+            </div>
+            <div className="cartitems-promocode">
+                <p>If you have a promo code, Enter it here</p>
+                <div className="cartitems-promobox">
+                    <input type="text" placeholder='promo code'/>
+                    <button>Submit</button>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
